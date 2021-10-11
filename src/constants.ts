d02ef736dd804eaf8faaa2e127552407f7152c12
Ivan Čurić <ivan.curic@gmail.com>
2021-10-11 13:10:28 +0200

    Remove files (reverting later for code comments)

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-import { Direction } from './interfaces';
-
-export const START_VALUE = '@';
-export const END_VALUE = 'x';
-export const VERTICAL_VALUE = '|';
-export const HORIZONTAL_VALUE = '-';
-export const TURN_VALUE = '+';
-
-export const ALL_DIRECTIONS: Direction[] = [Direction.Up, Direction.Right, Direction.Down, Direction.Left];
--- src/utils/validationUtil.ts
+++
@@ -1,8 +0,0 @@
-import { getCharFrequency } from './stringUtil';
-
-export const validateCharFrequency = (str: string, char: string, frequency: number): void => {
-  const charCount = getCharFrequency(str, char);
-  if (charCount !== frequency) {
-    throw new Error(`There should be exactly ${frequency} ${char} characters. Found: ${charCount}.`);
-  }
-};
